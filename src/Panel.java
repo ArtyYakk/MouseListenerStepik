2bcf1539e7b861b1fa9f0cc01037de89b275e3bc
iakon <iakonien@gmail.com>
2022-08-25 22:56:22 +0300

    можно двигать предметы

--- src/Panel.java
+++ src/Panel.java
@@ -1,8 +1,8 @@
 import javax.swing.*;
-import javax.swing.text.StringContent;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 
@@ -10,12 +10,14 @@ public class Panel extends JPanel {
     private ArrayList objects;
     private ArrayList colors;
     private Rectangle2D current;
+    private boolean rightBut = false;
 
     public Panel(){
         objects = new ArrayList();
         colors = new ArrayList();
         current = null;
         addMouseListener(new MyMouse());
+        addMouseMotionListener(new MyDrag());
     }
 
     @Override
@@ -47,10 +49,41 @@ public class Panel extends JPanel {
     private class MyMouse extends MouseAdapter{
         @Override
         public void mousePressed(MouseEvent e) {
-            if(e.getButton() == 1) add(e.getPoint());
-            else if(e.getButton() == 3) delete(e.getPoint());
-        }
+            if(e.getButton() == 3) rightBut = true;
+            else rightBut = false;
 
+            boolean check = true;
+            if(e.getButton() == 1){ //Нажал на ЛКМ
+                for(int i=0; i<objects.size(); i++){
+                    if(((Rectangle2D)objects.get(i)).contains(e.getX(),e.getY())){
+                        check = false;
+                        break;
+                    }
+                }
+                if(check) add(e.getPoint());
+            }
+            else if(e.getButton() == 2) delete(e.getPoint()); //Нажал на СКМ
+        }
     }
+
+    private class MyDrag extends MouseMotionAdapter {
+        @Override
+        public void mouseDragged(MouseEvent e) {
+            if(rightBut){
+                for(int i=0; i<objects.size(); i++){
+                    if(((Rectangle2D)objects.get(i)).contains(e.getPoint())){
+                        Color preColor = (Color)colors.get(i);
+                        colors.remove(i);
+                        objects.remove(i);
+                        current = new Rectangle2D.Double(e.getX()-10,e.getY()-10,20,20);
+                        objects.add(current);
+                        colors.add(preColor);
+                        repaint();
+                    }
+                }
+            }
+        }
+    }
+
 
 }
